55394b37764ab19dc575d7518b14305bbf407e81
Bernard Bill <117160764+bill-sparkx@users.noreply.github.com>
2024-07-19 04:16:13 +0000

    Set up expo router

---
+++ src/app/index.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import exercises from '../../assets/data/exercises.json';
+import ExerciseListItem from '../../src/components/ExerciseListItem';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+      data={exercises}
+      contentContainerStyle={{gap: 5}}
+      keyExtractor={(item, index)=> item.name + index}
+      renderItem={({item}) => <ExerciseListItem  item={item}/>}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'ghostwhite',
+  },
+});
---
+++ src/components/ExerciseListItem.jsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+export default function ExerciseListItem({item}) {
+    return(
+      <View style={styles.excerciseContainer}>
+        <Text style={styles.exerciseName}>
+          {item.name}
+        </Text>
+        <Text style={styles.exerciseSubtitle}>
+          {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
+        </Text>
+    </View>
+    )
+  }
+
+  const styles = StyleSheet.create({
+    excerciseContainer:{
+      backgroundColor: '#fff',
+      padding: 10,
+      borderRadius: 10, 
+      gap: 5,
+      marginHorizontal: 3,
+// shadow
+shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 1,
+},
+shadowOpacity: 0.20,
+shadowRadius: 1.41,
+
+elevation: 2,
+  
+    },
+   
+    exerciseName: {
+      fontSize: 20,
+      fontWeight: '500',
+    },
+    exerciseSubtitle:{
+      color: 'dimgray',
+      
+    },
+  });
+  
